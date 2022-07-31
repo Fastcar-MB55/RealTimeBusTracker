0dab8688f46b87c1f6f9a6abe954329b0611610d
adluce2 <103570169+adluce2@users.noreply.github.com>
2022-07-31 12:06:56 -0400

    Update JS

--- bustracker_html.js
+++ bustracker_html.js
@@ -104,7 +104,7 @@ async function run(){
 
 // Request bus data from MBTA
 async function getBusLocations(){
-	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=77&include=trip';
+	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
 	const response = await fetch(url);
 	const json     = await response.json();
 	return json.data;
